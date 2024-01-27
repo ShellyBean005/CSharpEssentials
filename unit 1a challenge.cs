078cd11cd7c1449dac246813acec336a56bcca2d
ShellyBean005 <143045243+ShellyBean005@users.noreply.github.com>
2024-01-26 21:09:20 -0700

    unit 1a challenge

---
+++ unit 1a challenge.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_1a_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a Bad Calculator! Where we only work in whole numbers! Please enter your first digit.");
+
+            string firstDig = Console.ReadLine();
+            
+            int FirstIn = Convert.ToInt32(firstDig);
+
+            Console.WriteLine("Please enter your second digit.");
+
+            string secDig = Console.ReadLine();
+
+            int SecIn = Convert.ToInt32(secDig);
+
+            Console.WriteLine("What would you like to do with these digits? Limit yourseft to + - * /");
+
+            string WhatDo = Console.ReadLine();
+
+            if (WhatDo.Equals("+"))
+            {
+
+                Console.WriteLine(FirstIn + SecIn);
+
+            }
+            else if (WhatDo.Equals("-"))
+            {
+                Console.WriteLine(FirstIn - SecIn);
+
+            }
+            else if (WhatDo.Equals("*"))
+            {
+                Console.WriteLine(FirstIn * SecIn);
+            }
+            else if (WhatDo.Equals("/"))
+            {
+                Console.WriteLine(FirstIn / SecIn);
+            }
+            else
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
